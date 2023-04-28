05e1ebc49fe397976235c062ebc7e6b818754e36
a22marca <128985931+a22marca@users.noreply.github.com>
2023-04-28 13:23:17 +0200

    Skapat Mountain klass (RecycleView item).

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,15 @@
+package com.example.networking;
+
+public class Mountain {
+    private String name;
+    private int height;
+    private String location;
+
+    public Mountain(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
