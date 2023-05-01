538345a9657d3127e0f900ffeb8b388d03989f12
a22marca <128985931+a22marca@users.noreply.github.com>
2023-05-01 14:54:14 +0200

    Lagt till fler attribut till Mountain

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,12 +1,24 @@
 package com.example.networking;
 
+import android.media.Image;
+
 public class Mountain {
     private String name;
-    private int height;
     private String location;
+    private int elevationM;
+    private int elevationFt;
+    private String wikiLink;
 
     public Mountain(String name) {
         this.name = name;
+    }
+
+    public Mountain(String name, String location, int elevationM, int elevationFt, String wikiLink) {
+        this.name = name;
+        this.location = location;
+        this.elevationM = elevationM;
+        this.elevationFt = elevationFt;
+        this.wikiLink = wikiLink;
     }
 
     public String getName() {
